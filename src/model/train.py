b0fad09e725d8d8e84f4a3688531f6bf4fe36f22
rthoma43 <123902962+rthoma43@users.noreply.github.com>
2023-07-25 16:57:54 -0400

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -41,8 +41,8 @@ def split_data(df):
                'TricepsThickness', 'SerumInsulin', 'BMI',
                'DiabetesPedigree', 'Age']].values,
     df['Diabetic'].values
-    X_train, X_test, y_train, y_test = train_test_split(X, y,
-                                                        test_size=0.30, random_state=0)
+    X_train, X_test, y_train, y_test = 
+    train_test_split(X, y,test_size=0.30, random_state=0)
     return X_train, X_test, y_train, y_test
 
 
